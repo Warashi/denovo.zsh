255a8f004c81020cc619da9ed2c66e0ec0463e98
Shinnosuke Sawada <6warashi9@gmail.com>
2023-05-20 20:31:41 +0900

    fix: listenが間に合わないことがあったので順番を修正

--- denovo.plugin.zsh
+++ denovo.plugin.zsh
@@ -17,9 +17,9 @@ fi
 
 export DENOVO_PATH=${DENOVO_ROOT}
 
+source "${DENOVO_ROOT}/shell/listen.zsh"
 source "${DENOVO_ROOT}/shell/dispatch.zsh"
 source "${DENOVO_ROOT}/shell/register.zsh"
-source "${DENOVO_ROOT}/shell/listen.zsh"
 
 function denovo-stop-server() {
   local pid="$(denovo_dispatch pid | jq .result)"
