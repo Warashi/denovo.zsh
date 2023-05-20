51f18bfaead640ed2c9be04f9397138a79942611
Shinnosuke Sawada <6warashi9@gmail.com>
2023-05-20 21:34:59 +0900

    fix: retry max 3 times for dispatch

--- shell/dispatch.zsh
+++ shell/dispatch.zsh
@@ -5,16 +5,25 @@ function denovo_dispatch() {
 		params='[]'
 	fi
 	local request=$(jq -c -n --arg method "${method}" --argjson params "${params}" '{jsonrpc:"2.0",id:1,method:$method,params:$params}')
-	_denovo_dispatch "$request"
+	_denovo_dispatch "$request" 0
 }
 
 function _denovo_dispatch() {
+	local request=$1
+	local retry=$2
 	local REPLY
 	local -i isok fd
 	zmodload zsh/net/socket
 	zsocket "$DENOVO_DENO_SOCK"
 	isok=$?
-	((isok == 0)) || return 1
+	if ((isok != 0)); then
+		if ((retry > 3)); then
+			return 1
+		fi
+		sleep 0.1
+		((retry++))
+		_denovo_dispatch "$request" $retry
+	fi
 	fd=$REPLY
 	echo "$1" >&$fd
 	cat <&$fd
