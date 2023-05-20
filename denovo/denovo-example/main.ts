863e84c17b47d0b05336e9c78b0d69e96502191d
Shinnosuke Sawada <6warashi9@gmail.com>
2023-05-20 21:42:34 +0900

    chore: trim response

--- denovo/denovo-example/main.ts
+++ denovo/denovo-example/main.ts
@@ -2,5 +2,5 @@ import { Denovo } from "../@denovo/mod.ts";
 
 export async function main(denovo: Denovo): Promise<void> {
   const result = await denovo.eval("echo hello world");
-  console.log(result);
+  console.log(result.trim());
 }
