39ecdbeaad8c6a735c0881c32135188b53b537fc
Shinnosuke Sawada <6warashi9@gmail.com>
2023-05-21 10:30:58 +0900

    refactor: move notify funcs

--- shell/notify.zsh
+++ shell/notify.zsh
@@ -1,14 +1,14 @@
-function denovo_dispatch() {
+function denovo_notify() {
 	local method=$1
 	local params=$2
 	if [[ -z "$params" ]]; then
 		params='[]'
 	fi
   local request="$(_denovo_request "$method" "$params")"
-	_denovo_dispatch "$request" 0
+	_denovo_notify "$request" 0
 }
 
-function _denovo_dispatch() {
+function _denovo_notify() {
 	local request=$1
 	local retry=$2
 	local REPLY
@@ -22,10 +22,11 @@ function _denovo_dispatch() {
 		fi
 		sleep 0.01
 		((retry++))
-		_denovo_dispatch "$request" $retry
+		_denovo_notify "$request" $retry
+		return $?
 	fi
 	fd=$REPLY
 	echo "$1" >&$fd
-	cat <&$fd
 	exec {fd}>&-
 }
+
