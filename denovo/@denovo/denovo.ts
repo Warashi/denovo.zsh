69222a66c69ac14b928269e5a93c9d187bc6f335
Shinnosuke Sawada <6warashi9@gmail.com>
2023-05-24 08:29:24 +0900

    docs: add denovo api docs

--- denovo/@denovo/denovo.ts
+++ denovo/@denovo/denovo.ts
@@ -3,15 +3,51 @@ export interface Dispatcher {
 }
 
 export interface Meta {
+  /**
+   * application mode
+   */
   readonly mode: "release" | "debug" | "test";
+
+  /**
+   * zsh version
+   */
   readonly version: string;
+
+  /**
+   * host platform
+   */
   readonly platform: "windows" | "mac" | "linux";
 }
 
 export interface Denovo {
+  /**
+   * plugin name
+   */
   readonly name: string;
+
+  /**
+   * meta information
+   */
   readonly meta: Meta;
+
+  /**
+   * dispatcher registered by plugin
+   */
   dispatcher: Dispatcher;
+
+  /**
+   * eval script in zsh
+   *
+   * @param script script to eval
+   */
   eval(script: string): Promise<string>;
+
+  /**
+   * dispatch function to other plugins
+   *
+   * @param name plugin name
+   * @param fn function name
+   * @param args function arguments
+   */
   dispatch(name: string, fn: string, ...args: unknown[]): Promise<unknown>;
 }
