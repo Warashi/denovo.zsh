2de4f0bd9d775e241808c95d29fdec5d6e535d11
Shinnosuke Sawada <6warashi9@gmail.com>
2023-05-24 07:47:56 +0900

    chore: ignore require-await

--- denovo/denovo-example/main.ts
+++ denovo/denovo-example/main.ts
@@ -4,6 +4,7 @@ export async function main(denovo: Denovo): Promise<void> {
   const result = await denovo.eval("echo hello world");
   console.log(result.trim());
   denovo.dispatcher = {
+    // deno-lint-ignore require-await
     async echo(...args: unknown[]): Promise<unknown[]> {
       return args;
     },
