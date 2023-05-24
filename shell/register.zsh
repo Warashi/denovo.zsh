378b7337e0df0fa0032ec801e41703e0f879c3e5
Shinnosuke Sawada <6warashi9@gmail.com>
2023-05-25 06:57:02 +0900

    feat: add meta information to register

--- shell/register.zsh
+++ shell/register.zsh
@@ -1,8 +1,32 @@
 function denovo-register() {
 	local plugin=$1
 	local script=$2
-	local request="$(_denovo_json_array $(_denovo_json_string $plugin) $(_denovo_json_string $script))"
+	local meta=$(_denovo_meta)
+	local request="$(_denovo_json_array $(_denovo_json_string $plugin) $(_denovo_json_string $script) $meta)"
 	_denovo_dispatch "register" "$request" >/dev/null
+}
+
+function _denovo_meta() {
+	_denovo_json_object \
+		$(_denovo_json_kv "mode" $(_denovo_json_string "release")) \
+		$(_denovo_json_kv "version" $(_denovo_json_string "$ZSH_VERSION")) \
+		$(_denovo_json_kv "platform" $(_denovo_json_string $(_denovo_host_platform)))
+}
+
+function _denovo_host_platform() {
+	if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+		echo "linux"
+	elif [[ "$OSTYPE" == "darwin"* ]]; then
+		echo "mac"
+	elif [[ "$OSTYPE" == "cygwin" ]]; then
+		echo "windows"
+	elif [[ "$OSTYPE" == "msys" ]]; then
+		echo "windows"
+	elif [[ "$OSTYPE" == "win32" ]]; then
+		echo "windows"
+	else
+		echo "unknown"
+	fi
 }
 
 function _denovo_discover() {
