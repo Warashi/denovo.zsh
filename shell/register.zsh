e192fb30cf9ba53e02ac97644c1cb8750181d363
Shinnosuke Sawada <6warashi9@gmail.com>
2024-02-20 23:00:46 +0900

    refactor(shell): implement mini jo with zsh

--- shell/register.zsh
+++ shell/register.zsh
@@ -3,15 +3,11 @@ function denovo-register() {
 	local directory=$2
 	local script=$3
 	local meta="$(_denovo_meta)"
-	local request="$(_denovo_json_array "$(_denovo_json_string "$plugin")" "$(_denovo_json_string "$directory")" "$(_denovo_json_string "$script")" "$meta")"
-	_denovo_notify "register" "$request" >/dev/null
+	_denovo_notify '' "register" "$plugin" "$directory" "$script" "$meta" >/dev/null
 }
 
 function _denovo_meta() {
-	_denovo_json_object \
-		"$(_denovo_json_kv "mode" "$(_denovo_json_string "release")")" \
-		"$(_denovo_json_kv "version" "$(_denovo_json_string "$ZSH_VERSION")")" \
-		"$(_denovo_json_kv "platform" "$(_denovo_json_string "$(_denovo_host_platform)")")"
+	_denovo_jo -s mode=release -s version="$ZSH_VERSION" -s platform="$(_denovo_host_platform)"
 }
 
 function _denovo_host_platform() {
